a29e48fc94771b166a98f971787ab6ac657568d3
m1n3g <68592740+m1n3g@users.noreply.github.com>
2025-09-04 19:18:27 +0800

    #

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudPlayground;
 using MudBlazor.Services;
+using MudBlazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
